c410c4311ec8ebfeb1d8902e5c4666ba36dc3325
Paras Khanchandani <paras_theloveable@hotmail.com>
2022-10-08 12:12:06 -0400

    Initial Commit

---
+++ Nitroless/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Nitroless
+//
+//  Created by Paras KCD on 2022-10-08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
